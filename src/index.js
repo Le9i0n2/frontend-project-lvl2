c7d6443fc54db9fdd48fef78056aa6a1b288ab70
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-06-12 21:48:03 +0300

    Add more options for gendiff

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import { Command } from 'commander';
 export default () => {
   const program = new Command();
 
+  program.configureHelp({
+    sortOptions: false,
+  });
+
   program.name('gendiff');
 
   program
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number', ',');
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format');
 
   program.parse();
 };
