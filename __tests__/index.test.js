a94f6e4127e4471a644f1afefda69ef6ad7393af
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-07-07 16:28:35 +0300

    Add genDiff test for .yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,8 +15,14 @@ const getFixturePath = (filename) =>
 const rightAnswer =
   '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
-test('genDiff', () => {
+test('genDiff .json', () => {
   expect(
     genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
+  ).toBe(rightAnswer);
+});
+
+test('genDiff .yaml', () => {
+  expect(
+    genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml')),
   ).toBe(rightAnswer);
 });
