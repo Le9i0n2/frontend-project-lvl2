4c462042d4776afa5758a821a79fe7f378d27970
Vasiliy Dontzov <djchevicheis@gmail.com>
2022-07-07 15:22:27 +0300

    Add tests for project

--- Makefile
+++ Makefile
@@ -15,3 +15,9 @@ setup: # Install dependencies and make the gendiff command available
 
 lint: # Start eslint check
 	npx eslint .
+
+test: # Run all tests
+	NODE_OPTIONS=--experimental-vm-modules npm test
+
+testCoverage: # Check tests coverage
+	NODE_OPTIONS=--experimental-vm-modules npm test -- --coverage 
---
+++ __tests__/index.test.js
@@ -0,0 +1,22 @@
+// test for genDiff
+
+import path from 'node:path';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+
+const rightAnswer =
+  '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+
+test('genDiff', () => {
+  expect(
+    genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
+  ).toBe(rightAnswer);
+});
